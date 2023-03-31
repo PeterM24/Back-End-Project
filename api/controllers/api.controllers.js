0cf1de26c322ca4309a659f0a405fe3d0c4e5819
Peter Matthews <peterjmatthews@me.com>
2023-03-31 11:27:58 +0100

    clean up formatting

--- api/controllers/api.controllers.js
+++ api/controllers/api.controllers.js
@@ -1,6 +1,6 @@
-const { fetchApiEndpoints } = require("../models/api.models")
+const { fetchApiEndpoints } = require("../models/api.models");
 
 exports.getApiEndpoints = async (req, res, next) => {
-    const endpoints = await fetchApiEndpoints();
-    res.status(200).send({endpoints})
-}
+  const endpoints = await fetchApiEndpoints();
+  res.status(200).send({ endpoints });
+};
--- api/models/api.models.js
+++ api/models/api.models.js
@@ -1,6 +1,9 @@
 const fs = require("fs/promises");
 
 exports.fetchApiEndpoints = async () => {
-  const endpoints = await fs.readFile(`${__dirname}/../../endpoints.json`, `utf-8`);
+  const endpoints = await fs.readFile(
+    `${__dirname}/../../endpoints.json`,
+    `utf-8`
+  );
   return endpoints;
 };
