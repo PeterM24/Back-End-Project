0e98743d0a4d9d3ed632e13d38712232544c2096
Peter Matthews <peterjmatthews@me.com>
2023-04-23 17:01:24 +0100

    update dev data - remove category hyphens

--- db/data/development-data/categories.js
+++ db/data/development-data/categories.js
@@ -5,7 +5,7 @@ module.exports = [
       'Strategy-focused board games that prioritise limited-randomness'
   },
   {
-    slug: 'hidden-roles',
+    slug: 'hidden roles',
     description:
       "One or more players around the table have a secret, and the rest of you need to figure out who! Players attempt to uncover each other's hidden role"
   },
@@ -15,22 +15,22 @@ module.exports = [
       'Games involving physical skill, something like Gladiators, for Board Games!'
   },
   {
-    slug: 'push-your-luck',
+    slug: 'push your luck',
     description:
       'Games that allow you to take bigger risks to achieve increasingly valuable rewards - or to decide to keep what you’ve got before you lose everything.'
   },
   {
-    slug: 'roll-and-write',
+    slug: 'roll and write',
     description:
       'Roll some dice and decide how to use the outcome, writing it into a personal scoring sheet. '
   },
   {
-    slug: 'deck-building',
+    slug: 'deck building',
     description:
       'Games where players construct their own deck as a main element of the gameplay'
   },
   {
-    slug: 'engine-building',
+    slug: 'engine building',
     description:
       'Games where players construct unique points-gaining engines main element of the gameplay'
   }
